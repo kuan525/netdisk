81dd198916a80c581cfd623d4fc42a632d2fba1a
mac_kuan <kuan_525@163.com>
2023-07-15 00:49:59 +0800

    封装dbclient

--- service/download/api/download.go
+++ service/download/api/download.go
@@ -2,26 +2,11 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
-	"netdisk/common"
-	dbcli "netdisk/dbclient"
+	dbcli "github.com/kuan525/netdisk/dbclient"
 )
 
-// DownloadURLHandler 生成文件的下载地址
 func DownloadURLHandler(c *gin.Context) {
 	filehash := c.Request.FormValue("filehash")
-	// 从文件表查找记录
-	dbResp, err := dbcli.GetFileMeta(filehash)
-	if err != nil {
-		c.JSON(
-			http.StatusOK,
-			gin.H{
-				"code": common.StatusServerError,
-				"msg":  "server error",
-			})
-		return
-	}
 
-	tblFile := dbcli.ToTableFile(dbResp.)
-
+	dbResp, err := dbcli.
 }
