5867f02ba3d0cacfb15294dd41c7388b4e10c6ba
mac_kuan <kuan_525@163.com>
2023-07-22 15:33:12 +0800

    使用urfave/cli/v2库自定义命令行参数

--- common/flags.go
+++ common/flags.go
@@ -1,25 +1,27 @@
 package common
 
-import "github.com/micro/cli"
+import (
+	"github.com/urfave/cli/v2"
+)
 
 // CustomFlags 自定义命令行参数
 var CustomFlags = []cli.Flag{
-	cli.StringFlag{
+	&cli.StringFlag{
 		Name:  "db-host",
 		Value: "127.0.0.1",
 		Usage: "database address",
 	},
-	cli.StringFlag{
+	&cli.StringFlag{
 		Name:  "mq-host",
 		Value: "127.0.0.1",
 		Usage: "mq(rabbitmq) address",
 	},
-	cli.StringFlag{
+	&cli.StringFlag{
 		Name:  "cache-host",
 		Value: "127.0.0.1",
 		Usage: "cache(redis) address",
 	},
-	cli.StringFlag{
+	&cli.StringFlag{
 		Name:  "ceph-host",
 		Value: "127.0.0.1",
 		Usage: "ceph address",
