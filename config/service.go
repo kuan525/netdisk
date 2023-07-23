ab787f4d879ad13b281e3647e0d80333f13f2806
mac_kuan <kuan_525@163.com>
2023-07-23 17:34:44 +0800

    bug

--- config/service.go
+++ config/service.go
@@ -7,12 +7,8 @@ const (
 	// UploadEntry 配置上传入口地址
 	UploadEntry = "localhost:28080"
 	// UploadServiceHost 上传服务监听地址
-	UploadServiceHost = "localhost:9090"
-	// UploadLBHost 上传服务LB地址
-	UploadLBHost = "http://upload.netdisk.com"
+	UploadServiceHost = "localhost:28080"
 
-	// DownloadLBHost 下载服务LB地址
-	DownloadLBHost = "http://download.netdisk.com"
 	// DownloadEntry 配置下载入口地址
 	DownloadEntry = "localhost:38080"
 	// DownloadServiceHost 下载服务监听的地址
